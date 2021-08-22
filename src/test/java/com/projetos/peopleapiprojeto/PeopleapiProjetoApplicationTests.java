4744828682d10a425a487baec6788f27bd5d0f33
RafaelFerreiraNunes <rafaelguarulhos@hotmail.com>
2021-08-22 16:29:14 -0300

    ajustando versão java

---
+++ src/test/java/com/projetos/peopleapiprojeto/PeopleapiProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetos.peopleapiprojeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleapiProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
